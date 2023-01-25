754531ed9b838a1624f3faf4bcb975cb755b9a18
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-01-26 01:43:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// Javascript Assignment 1
+
+// answer no 1
+// swal("Hello , how are you sir.")
+
+
+// answer no 2
+// swal("Error, Please enter a valid password")
+
+
+// answer no 3
+// alert("Welcome to JS Land.... \n Happy Coding")
+
+
+// answer no 4
+// alert("Welcome to JS Land")
+// alert("Happy Coding")
+
+
+// answer no 5
+// alert("Hello... I can run my JS through my web browser's console")
+
+
+// answer no 6 is in html file
+
+// answer no 7 is also in html file
